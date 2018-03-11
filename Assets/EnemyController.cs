c540b5cd3f4968147b26ac5b34a25f1937bffaa1
Artem Shvadskiy <shpen72991@gmail.com>
2018-03-10 16:01:38 -0800

    Add simple enemy that follows player

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class EnemyController : MonoBehaviour {
+	private NavMeshAgent _navAgent;
+	private GameObject _player;
+
+	// Use this for initialization
+	void Start () {
+		_navAgent = GetComponent<NavMeshAgent>();
+		_player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_navAgent.SetDestination(_player.transform.position);
+	}
+}
